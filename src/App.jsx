e68ce62eba16dbd7407d3943c54da382baf0aed5
Shivanigupta2015 <guptashivani7409@gmail.com>
2025-06-23 06:23:21 -0400

    1.1

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,19 @@ function App() {
           THE PROTECTOR KIT
         </h1>
       </div>  
-      <div className="bg-white m-3 mt-30 p-4 max-w-max rounded shadow-orange-400 shadow-lg ">
-        <div className=" flex flex-wrap gap-3 justify-center items-center">
-          <img src="/img/CleanGuard.webp" alt="img" className="w-24 sm:w-28 md:w-32 rounded-xl shadow-md transition-all duration-500 ease-in-out hover:scale-105 hover:shadow-xl hover:ring-2 hover:ring-sky-300 shadow-sky-800" />
-          <img src="/img/RideGuard.webp" alt="img" className="w-24 sm:w-28 md:w-32 rounded-xl shadow-md transition-all duration-500 ease-in-out hover:scale-105 hover:shadow-xl hover:ring-2 hover:ring-yellow-300 shadow-amber-700" />
-          <img src="/img/MonsoonGuard.webp" alt="img" className="w-24 sm:w-28 md:w-32 rounded-xl shadow-md transition-all duration-500 ease-in-out hover:scale-105 hover:shadow-xl hover:ring-2 hover:ring-sky-300 shadow-sky-700" />
-          <img src="/img/ToolShield.webp" alt="img" className="w-24 sm:w-28 md:w-32 rounded-xl shadow-md transition-all duration-500 ease-in-out hover:scale-105 hover:shadow-xl hover:ring-2 hover:ring-red-300 shadow-red-700" />
-          <img src="/img/CommuteShield.webp" alt="img" className="w-24 sm:w-28 md:w-32 rounded-xl shadow-md transition-all duration-500 ease-in-out hover:scale-105 hover:shadow-xl hover:ring-2 hover:ring-black shadow-black" />
-          <img src="/img/GardenShield.webp" alt="img" className="w-24 sm:w-28 md:w-32 rounded-xl shadow-md transition-all duration-500 ease-in-out hover:scale-105 hover:shadow-xl hover:ring-2 hover:ring-green-300 shadow-green-800"/>
-          <img src="/img/TechGuard.webp" alt="img" className="w-24 sm:w-28 md:w-32 rounded-xl shadow-md transition-all duration-500 ease-in-out hover:scale-105 hover:shadow-xl hover:ring-2 hover:ring-blue-300 shadow-blue-700 "/>
-          <img src="/img/KitchenArmor.webp" alt="img" className="w-24 sm:w-28 md:w-32 rounded-xl shadow-md transition-all duration-500 ease-in-out hover:scale-105 hover:shadow-xl hover:ring-2 hover:ring-orange-300 shadow-orange-700 " />
-          <img src="/img/SummerShield.webp" alt="img" className="w-24 sm:w-28 md:w-32 rounded-xl shadow-md transition-all duration-500 ease-in-out hover:scale-105 hover:shadow-xl hover:ring-2 hover:ring-yellow-300 shadow-yellow-700 " />
-          <img src="/img/WinterGuard.webp" alt="img" className="w-24 sm:w-28 md:w-32 rounded-xl shadow-md transition-all duration-500 ease-in-out hover:scale-105 hover:shadow-xl hover:ring-2 hover:ring-sky-300 shadow-sky-700" />
-
+      <div className="bg-white min-w-auto m-3 mt-30 p-3 rounded shadow-orange-400 shadow-lg ">
+        <div className=" flex flex-wrap gap-4 justify-center items-center">
+          <img src="/img/CleanGuard.webp" alt="img" className="w-24 sm:w-28 md:w-28 rounded-xl shadow-md transition-all duration-500 ease-in-out hover:scale-105 hover:shadow-xl hover:ring-2 hover:ring-sky-300 shadow-sky-800" />
+          <img src="/img/RideGuard.webp" alt="img" className="w-24 sm:w-28 md:w-28 rounded-xl shadow-md transition-all duration-500 ease-in-out hover:scale-105 hover:shadow-xl hover:ring-2 hover:ring-yellow-300 shadow-amber-700" />
+          <img src="/img/MonsoonGuard.webp" alt="img" className="w-24 sm:w-28 md:w-28 rounded-xl shadow-md transition-all duration-500 ease-in-out hover:scale-105 hover:shadow-xl hover:ring-2 hover:ring-sky-300 shadow-sky-700" />
+          <img src="/img/ToolShield.webp" alt="img" className="w-24 sm:w-28 md:w-28 rounded-xl shadow-md transition-all duration-500 ease-in-out hover:scale-105 hover:shadow-xl hover:ring-2 hover:ring-red-300 shadow-red-700" />
+          <img src="/img/CommuteShield.webp" alt="img" className="w-24 sm:w-28 md:w-28 rounded-xl shadow-md transition-all duration-500 ease-in-out hover:scale-105 hover:shadow-xl hover:ring-2 hover:ring-black shadow-black" />
+          <img src="/img/GardenShield.webp" alt="img" className="w-24 sm:w-28 md:w-28 rounded-xl shadow-md transition-all duration-500 ease-in-out hover:scale-105 hover:shadow-xl hover:ring-2 hover:ring-green-300 shadow-green-800"/>
+          <img src="/img/TechGuard.webp" alt="img" className="w-24 sm:w-28 md:w-28 rounded-xl shadow-md transition-all duration-500 ease-in-out hover:scale-105 hover:shadow-xl hover:ring-2 hover:ring-blue-300 shadow-blue-700 "/>
+          <img src="/img/KitchenArmor.webp" alt="img" className="w-24 sm:w-28 md:w-28 rounded-xl shadow-md transition-all duration-500 ease-in-out hover:scale-105 hover:shadow-xl hover:ring-2 hover:ring-orange-300 shadow-orange-700 " />
+          <img src="/img/SummerShield.webp" alt="img" className="w-24 sm:w-28 md:w-28 rounded-xl shadow-md transition-all duration-500 ease-in-out hover:scale-105 hover:shadow-xl hover:ring-2 hover:ring-yellow-300 shadow-yellow-700 " />
+          <img src="/img/WinterGuard.webp" alt="img" className="w-24 sm:w-28 md:w-28 rounded-xl shadow-md transition-all duration-500 ease-in-out hover:scale-105 hover:shadow-xl hover:ring-2 hover:ring-sky-300 shadow-sky-700" />
         </div>
-      
       </div>
       </div>
 
